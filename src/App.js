4a827987e2cb875fb6762db3da5cddb635b64dc9
liamtft <liam@tsangs.us>
2023-06-30 09:56:56 -0400

    Buffer update

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends React.Component {
   render() {
     return (
       <BaseScene>   
-        <MovingAudioSource direction={1} url="/assets/music/aphro_mono_comp.wav"></MovingAudioSource>
-        <MovingAudioSource direction={-1} url="/assets/music/eros_mono_comp.wav"></MovingAudioSource>
+        <MovingAudioSource direction={1} url="/assets/music/aphro_6ch_comp.wav"></MovingAudioSource>
+        <MovingAudioSource direction={-1} url="/assets/music/eros_6ch_comp.wav"></MovingAudioSource>
         <BaseBox text={false} position={[0, 0.5, 0]} args={[2, 0.25, 2]} color="white" />
 
         <BaseCharacter controls position={[0, 2, 0]} args={[0.5]} color="yellow" />
