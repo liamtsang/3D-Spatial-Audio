a6b69084617ae9e6eaaad5ebb6d4b697eeb0b5a1
liamtft <liam@tsangs.us>
2023-07-28 19:37:13 -0400

    Parametric

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
       <BaseScene>
         <Effects/>
         <MovingAudioSource url="/assets/music/aphro_6ch_comp.wav"></MovingAudioSource>
-        {/* <MovingAudioSource url="/assets/music/eros_6ch_comp.wav"></MovingAudioSource> */}
+        <MovingAudioSource url="/assets/music/eros_6ch_comp.wav"></MovingAudioSource>
         <BaseCharacter controls position={[0, 2, 0]} args={[0.5]} color="yellow" />
         <Clouds position={[0, -5, 0]} scale={[15,15,15]} rotation={[0,0,0]}/>
         {/* <GridBox position={[-10, 0, 0]} scale={[10,10,10]} rotation={[0,0,0]}/> */}
